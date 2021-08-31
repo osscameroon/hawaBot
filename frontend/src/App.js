4f0d18c6a17a1796e74d22a8489565207b520261
elhmn <nleme@live.fr>
2021-09-01 00:14:04 +0200

    fix: reformat App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,18 @@
 //import './styles/App.css';
-import Exit from './components/Exit';
-import Header from './components/Header';
-import PrevNext from './components/PrevNext';
-
-
-
-
+import Exit from "./components/Exit";
+import Header from "./components/Header";
+import PrevNext from "./components/PrevNext";
 
 function App() {
   return (
     <div className="App">
-       <Exit />
-       <Header text = {'Welcome to hawaBot. The official FAQ bot for Oss Cameroon. We provide an answer to all your questions about OssCameroon!'}/>
-      
-       <PrevNext />
-       
-       
-       
-      
-       
-       
-       
-      
-        
+      <Exit />
+      <Header
+        text={
+          "Welcome to hawaBot. The official FAQ bot for Oss Cameroon. We provide an answer to all your questions about OssCameroon!"
+        }
+      />
+      <PrevNext />
     </div>
   );
 }
