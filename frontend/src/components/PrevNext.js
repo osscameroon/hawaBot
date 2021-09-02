bbe5cfad59295f20c102934c0b86cf0b731b65f0
Zeroloader <danielsakou9@gmail.com>
2021-09-02 14:07:43 +0100

    Update PrevNext.js

--- frontend/src/components/PrevNext.js
+++ frontend/src/components/PrevNext.js
@@ -1,10 +1,29 @@
-import React, { useState, useCallback } from "react";
-import Data from "./data.json";
+import React, { useState, useCallback, useEffect } from "react";
+import faq from "./data.json";
 import "./styles/BodyStyle.css";
 
 const PrevNext = () => {
-  const [selectedID, setSelectedID] = useState(-1);
-  const toggleSelectedID = useCallback(
+  let [selectedID, setSelectedID] = useState(-1);
+  let [tab, setTab] = useState([]);
+
+  useEffect(()=>{
+    let currentTab = [];
+    faq.forEach(item => {
+      currentTab.push({
+        question: item.text,
+        answer: item.answer,
+      });
+      (item.children ?? []).forEach(item1 => {
+        currentTab.push({
+          question: item1.text,
+          answer: item1.answer,
+        });
+      });
+    });
+    setTab(currentTab);
+  });
+
+  /*const toggleSelectedID = useCallback(
     (index) => {
       if (selectedID === index) {
         setSelectedID(-1);
@@ -13,37 +32,43 @@ const PrevNext = () => {
       }
     },
     [selectedID]
-  );
+  );*/
 
-  const questionsAnswers = Data?.Questions?.map((question, index) => {
-    return {
-      question: question.text,
-      answer: Data.Answers[index].text,
-      id: index + 1,
-    };
-  });
+  // const questionsAnswers = faq?.map((faq, index) => {
+  //   return {
+  //     question: faq.text,
+  //     answer: faq.answer,
+  //     id: index + 1,
+  //   };
+  // });
 
-  let questionsAnswersHTML = questionsAnswers.map((questionAnswer, index) => (
-    <div key={index} className="question-answer">
-      <div
-        key={index}
-        className="question"
-        onClick={() => toggleSelectedID(index)}
-      >
-        <span key={index}>{questionAnswer.question}</span>
-        {index === selectedID && (
-          <span className="answer" key={index}>
-            {questionAnswer.answer}
-          </span>
-        )}
-        <span className="symbol-button">^</span>
-      </div>
-    </div>
-  ));
+  const questionsAnswersHTML = <></>;
 
   return (
     <div>
-      <div className="questions-frame">{questionsAnswersHTML}</div>
+      <div className="questions-frame">
+        {
+          tab.map((questionAnswer, index) => (
+            <div key={index} className="question-answer">
+              <div
+                className="question"
+                onClick={() => setSelectedID(index)}>
+                <span>
+                  {questionAnswer.question}
+                </span>
+                {
+                  index === selectedID ? (
+                    <span className="answer">
+                      {questionAnswer.answer}
+                    </span>
+                  ) : <></>
+                }
+                <span className="symbol-button">^</span>
+              </div>
+            </div>
+          ))
+        }
+      </div>
     </div>
   );
 };
