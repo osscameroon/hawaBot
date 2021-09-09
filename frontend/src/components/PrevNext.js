7665230f0364824315f49ec4e8a62b39f8babae8
elhmn <nleme@live.fr>
2021-09-09 17:49:48 +0200

    feat: recursive example

--- frontend/src/components/PrevNext.js
+++ frontend/src/components/PrevNext.js
@@ -1,10 +1,9 @@
-import React, { useState, useEffect,useCallback } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import faq from "./data.json";
 import "./styles/BodyStyle.css";
 
-const PrevNext = () => {
-  let [selectedID, setSelectedID] = useState(-1)
-  let [tab, setTab] = useState([]);
+const Faq = ({ faq }) => {
+  let [selectedID, setSelectedID] = useState(-1);
   const toggleSelectedID = useCallback(
     (index) => {
       if (selectedID === index) {
@@ -16,59 +15,43 @@ const PrevNext = () => {
     [selectedID]
   );
 
-  useEffect(()=>{
-    let currentTab = [];
-    faq.forEach((item, index) => {
-      currentTab.push({
-        question: item.text,
-        answer: item.answer
-      });
-      if(index === selectedID){
-        (item.children ?? []).forEach(item1 => {
-        currentTab.push({
-          question: item1.text,
-          answer: item1.answer,
-        });
-      });
-      }
-      
-    });
-    setTab(currentTab);
-  }, [selectedID]);
+  return faq.map((questionAnswer, index) => {
+    return (
+      <div className="questions-frame">
+        <div key={index} className="question-answer">
+          <div
+            className="question"
+            onClick={() => {
+              toggleSelectedID(index);
+            }}
+          >
+            <div>{questionAnswer.text}</div>
+            {
+              index === selectedID && (
+                <div key={index} className="answer">
+                  {questionAnswer.answer}
+                </div>
+              )
+            }
+            <span>^</span>
+          </div>
+            {
+              index === selectedID &&
+              questionAnswer.children &&
+              <Faq faq={questionAnswer.children}/>
+            }
+        </div>
+      </div>
+    );
+  });
+};
 
+const PrevNext = () => {
   return (
     <div>
-      <div className="questions-frame">
-        {
-          tab.map((questionAnswer, index) => (
-            <div key={index} className="question-answer">
-              <div
-                className="question"
-                onClick={() => { 
-                  toggleSelectedID(index);
-                }}>
-                  <div>
-                    {questionAnswer.question}
-                  </div>
-                  {
-                    index === selectedID ? (
-                      <div key={index} className="answer">
-                        {questionAnswer.answer}
-                      </div>
-                    ) : null
-                  }
-                  <span>^</span>
-                  
-              </div>
-            </div>
-          ))
-        }
-       
-      </div>
+      <Faq faq={faq} />
     </div>
-  )
-}
-
-
+  );
+};
 
 export default PrevNext;
